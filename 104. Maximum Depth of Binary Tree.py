0e7d8a899103ac9464be9c84e2eaffd22dc92dba
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-03-03 11:27:11 +0530

    daily solve

--- 104. Maximum Depth of Binary Tree.py
+++ 104. Maximum Depth of Binary Tree.py
@@ -10,3 +10,21 @@ class Solution(object):
         self.maxDepth(root=root.left,deepest=deepest)
         self.maxDepth(root=root.right,deepest=deepest)
         return self.deepest_
+
+#### BETTER VERSION ; AFTER LEARNING ABT QUES ; STILL NEED PRACTICE THO
+
+class Solution(object):
+    def maxDepth(self, root):
+        ans = 0
+        que = []
+        que.append(root)
+        while que:
+            ans += 1
+            for _ in range(len(que)):# exploring all the nodes at 1 level ; v can add append to ans to get back values too 
+                node = que.pop(0)
+                if node.left :
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+                
+        return ans
