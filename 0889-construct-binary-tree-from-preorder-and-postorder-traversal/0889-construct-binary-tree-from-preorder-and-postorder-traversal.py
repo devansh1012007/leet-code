f7ffa426ff1b644500df066c9020cbbdad7f9d67
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-03-22 12:11:14 +0530

    Time: 3 ms (67.65%), Space: 12.3 MB (88.24%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -5,21 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):    
-    def make_tree(self,left, right):
-            if left>right:
+    def make_tree(self, left, right):
+            if left > right:
                 return None
-            root = TreeNode(self.preorder[self.preord_idx])
-            self.preord_idx += 1
-            if left == right: # i forgot this in 1st try
+            
+            root = TreeNode(self.preorder.pop())
+            
+            if left == right: 
                 return root
-            mid = self.postodr_map[self.preorder[self.preord_idx]]
+            
+            mid = self.postodr_map[self.preorder[-1]]
+            
             root.left = self.make_tree(left, mid)
-            root.right = self.make_tree(mid + 1, right-1)
+            root.right = self.make_tree(mid + 1, right - 1)
+            
             return root
+
     def constructFromPrePost(self, preorder, postorder):
-        self.preord_idx = 0
-        self.postodr_map = {val:idx for idx, val in enumerate(postorder)}
-        self.preorder = preorder
-        self.postorder = postorder
-        return self.make_tree(0, len(preorder)-1)
+        self.postodr_map = {val: idx for idx, val in enumerate(postorder)}
         
+        self.preorder = preorder[::-1] 
+        
+        return self.make_tree(0, len(preorder) - 1)
