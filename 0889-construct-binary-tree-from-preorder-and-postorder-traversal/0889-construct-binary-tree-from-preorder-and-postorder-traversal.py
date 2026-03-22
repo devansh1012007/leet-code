28b7aa4de7fff00f5f123775d22fcf98a1442217
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-03-22 12:19:59 +0530

    Time: 7 ms (22.55%), Space: 12.3 MB (88.24%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -6,16 +6,12 @@
 #         self.right = right
 class Solution(object):    
     def make_tree(self, left, right):
-            if left == right: 
+            if left == right:# if elif reduced 3ms ! damn ; and pop was good 4 memory efficiency
                 return TreeNode(self.preorder.pop()) 
-            
             elif left > right:
                 return None
-            
             root = TreeNode(self.preorder.pop())
-            
             mid = self.postodr_map[self.preorder[-1]]
-            
             root.left = self.make_tree(left, mid)
             root.right = self.make_tree(mid + 1, right - 1)
             
@@ -23,7 +19,5 @@ class Solution(object):
 
     def constructFromPrePost(self, preorder, postorder):
         self.postodr_map = {val: idx for idx, val in enumerate(postorder)}
-        
-        self.preorder = preorder[::-1] 
-        
+        self.preorder = preorder[::-1]
         return self.make_tree(0, len(preorder) - 1)
