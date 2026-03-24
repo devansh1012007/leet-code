2313cdbaf28cf201c17d9e6e63b075f9fecca542
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-03-25 00:03:20 +0530

    Time: 0 ms (100%), Space: 13.8 MB (9.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -8,7 +8,7 @@ class Solution(object):# so baisly v take len(nums) now v have n ; now irritate 
             if i not in nums_set :
                 return i
 
-# you can do it with dick too but it's solw coz u r running a for loop 
+# you can do it with dict too but it's solw coz u r running a for loop 
 """
 def missingNumber(self, nums):
         n = len(nums)
