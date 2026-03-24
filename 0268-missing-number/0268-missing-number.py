a55b0c3c51a07d5891e9a86f853222e97dd762f9
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-03-25 00:01:09 +0530

    Time: 10 ms (36.19%), Space: 14.5 MB (6.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,11 @@
 class Solution(object):# so baisly v take len(nums) now v have n ; now irritate through the sorted array comparing it to range 
 # i not study bit multiplication 
-# another way of doing it will be to use set insted of compassion direcly to arr
+# another way of doing it will be to use hash  insted of compassion direcly to arr
     def missingNumber(self, nums):
         n = len(nums)
-        nums_set= set(nums)
+        hash_table = {val : index for index, val in enumerate (nums)}
         for i in range(0,n+1):
-            if i not in nums_set :
+            try :
+                a = hash_table[i]
+            except:
                 return i
