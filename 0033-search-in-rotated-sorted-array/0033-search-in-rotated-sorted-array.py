a34b1aa4a3963dd3e9154ec40e62183e413f0e03
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-03-31 08:46:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (99.11%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def BS(self, low, high, target,nums):
+    '''def BS(self, low, high, target,nums):
         while low <= high:
             mid = low + (high - low) // 2
             if nums[mid] == target:
@@ -26,11 +26,38 @@ class Solution(object):
                 else:
                     return self.BS(0,i-1,target,nums)
         
-        return self.BS(0,L,target,nums)
+        return self.BS(0,L,target,nums)'''
 
 # solution is not eligent , THINK !
 
 # Think how u can do it without making another loop !!!
 
 # so baicly by compairing mid and low and high v can tell which side is sorted and which is not
-# but v need to add anoth
+# but v need to add another if loop to check if sorted side is the one where target is 
+
+class Solution(object):
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            #  if left side sorted
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
