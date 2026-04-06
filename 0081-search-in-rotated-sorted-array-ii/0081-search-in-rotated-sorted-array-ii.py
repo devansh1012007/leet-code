4a2b80bf319982561405dc3ae706c06da67b599d
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-06 09:40:55 +0530

    Time: 0 ms (100%), Space: 12.6 MB (82.25%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,8 +1,70 @@
 class Solution(object):
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: bool
-        """
-        return target in set(nums)
+        high = len(nums)-1
+        low = 0
+        while high >= low:
+            while nums[low] == nums[high] and high - low > 2 :
+                if nums[low] == target == nums[high]:
+                    return True
+                else:
+                    low += 1
+                    high -= 1
+            mid = low + (high-low) //2
+            if nums[mid]== target:
+                return True
+            elif nums[mid] >= nums[low]:
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid -1
+                else : 
+                    low = mid + 1
+            elif nums[mid] <= nums[high]:
+                if target <= nums[high] and target > nums[mid]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            else:
+                if nums[mid] < target:
+                    low = mid + 1
+                else : 
+                    high = mid - 1
+
+        return False
+    
+
+    def BS(arr, lo, hi, x):
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if arr[mid] == x:
+                return mid
+            if arr[mid] < x:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        return -1
+
+    def findPivot(arr, lo, hi):
+        while lo <= hi:
+            if arr[lo] <= arr[hi]:
+                return lo
+
+            mid = (lo + hi) // 2
+            if arr[mid] > arr[hi]:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return lo
+    '''
+    def search(self,nums,target):
+        n = len(arr)
+        pivot = findPivot(arr, 0, n - 1)
+        if arr[pivot] == target:
+            return pivot
+
+        if pivot == 0:
+            return binarySearch(arr, 0, n - 1, target)
+
+        if arr[0] <= key:
+            return binarySearch(arr, 0, pivot - 1, target)
+        return binarySearch(arr, pivot + 1, n - 1, target)
+        '''
