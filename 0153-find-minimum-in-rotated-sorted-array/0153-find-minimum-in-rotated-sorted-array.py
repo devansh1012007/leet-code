2dc6baffb893f85bbf9f1ed8fa072224ec4d48eb
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-07 17:46:22 +0530

    Time: 0 ms (100%), Space: 12.4 MB (81.16%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -13,5 +13,5 @@ class Solution(object):
             if M > H:
                 low = mid + 1
             elif M < L:
-                high = mid 
+                high = mid # reason v don't use (mid-1) here is because v didn't verify the mid and also this is the same reason why v didb't set high>=low coz what if mid is the lowest then low pointer will come to high's index and the loop will stop
         return nums[low]
