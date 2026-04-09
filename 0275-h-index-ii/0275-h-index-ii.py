7c0ed9b68d5cb4bf7eefe9b202b524ae46a7ea5a
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-09 11:37:32 +0530

    Time: 0 ms (100%), Space: 15 MB (46.9%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -13,4 +13,4 @@ class Solution(object):
                 lo = mid+1
             else: 
                 high = mid
-        return min(citations[lo],L-lo) 
+        return min(citations[lo],L-lo) # how can i remove min?
