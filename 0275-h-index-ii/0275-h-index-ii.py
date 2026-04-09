a5d460fef518565ac2500584567e5d1716fdfd82
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-09 11:29:59 +0530

    Time: 0 ms (100%), Space: 15.1 MB (17.26%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def hIndex(self, citations):
+        L = len(citations) 
+        lo = 0
+        high = L-1
+        while high > lo:
+            mid = lo + (high-lo) //2
+            cit = citations[mid]
+            paper = L - mid
+            if cit == paper:
+                return paper
+            elif cit< paper:
+                lo = mid+1
+            else: 
+                high = mid
+        return min(citations[lo],L-lo) # need to chage
