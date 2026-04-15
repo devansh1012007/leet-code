28c27a62223ea704cd27e376814faa0e75037da9
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-15 19:22:53 +0530

    Time: 14 ms (65.04%), Space: 12.3 MB (91.24%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -1,4 +1,4 @@
-class Solution(object):
+'''class Solution(object):
     def firstBadVersion(self, n):
         hi = n
         lo = 1
@@ -9,7 +9,7 @@ class Solution(object):
             else:
                 lo = mid+1
         return hi
-# [1,2,3,4,5] --> [False, False, True, True, True]
+'''# [1,2,3,4,5] --> [False, False, True, True, True]
 
 ## Same same but different --> 
 """
@@ -25,3 +25,16 @@ class Solution(object):
                 hi = mid
         return hi
 """
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        hi = n
+        lo = 1
+        while hi >= lo:
+            mid = lo + (hi-lo) // 2
+            if isBadVersion(mid):
+                hi = mid-1
+            else:
+                lo = mid+1
+        return lo
+
