1d95ba5652e47b2efc8bd88ba210f8144a747647
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-21 00:23:30 +0530

    Time: 197 ms (34.98%), Space: 19.7 MB (99.68%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,22 +1,13 @@
-class MyCalendar(object):
+class MyCalendar:
 
     def __init__(self):
         self.booked =[]
+        
 
-    def book(self, startTime, endTime):
-        """
-        :type startTime: int
-        :type endTime: int
-        :rtype: bool
-        """       
+    def book(self, startTime: int, endTime: int) -> bool:
         for s,e in self.booked:
-            if not (endTime <= s or startTime >= e):  #(new_end <= old_start) or (new_start >= old_end)
+            if not (endTime <= s or startTime >= e):  
                 return False
-        # idk how to add and sort + do it with BS         
         self.booked.append((startTime,endTime))
         return True
 
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(startTime,endTime)
