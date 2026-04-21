fc29ef62f52a1ac50973d7d3298d5e8bf3dd5e54
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-21 13:52:25 +0530

    Time: 5 ms (98.66%), Space: 14.2 MB (14.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,11 @@
 import re
 class Solution(object):
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s = re.sub(r'[^a-zA-Z0-9]','', s)
+        s = re.sub(r'[^a-zA-Z0-9]','', s)# remove all the symbols and spaces
         s = s.lower()
-        print(s)
         i = 0
         j = len(s)-1
+        # 2 pointer in a loop
         while j>=i:
             if s[i] == s[j]:
                 i +=1 
