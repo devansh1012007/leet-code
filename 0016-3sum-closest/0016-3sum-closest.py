4c02807d5444d99139b25ec62b5d6f5a8f933018
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-26 18:14:40 +0530

    Time: 569 ms (5.89%), Space: 12.6 MB (10.29%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -6,20 +6,21 @@ class Solution(object):
         :rtype: int
         """
         # v can do it lil like BS 
+        lenght = len(nums) 
         nums.sort()
-        closest_sum = float('inf')
-        
-        for i in range(len(nums) - 2):
-            left, right = i + 1, len(nums) - 1
-            while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-                if abs(current_sum - target) < abs(closest_sum - target):
-                    closest_sum = current_sum
-                if current_sum < target:
-                    left += 1
-                elif current_sum > target:
-                    right -= 1
-                else:# if current == target
-                    return current_sum
-        
-        return closest_sum
+        closest = float('inf')
+        num_dict = {i: val for i, val in enumerate(nums)}
+        for i in range(lenght - 2):
+            lo = i + 1
+            hi = lenght -1
+            while hi > lo:
+                curr = num_dict[hi]+num_dict[lo]+num_dict[i]
+                if abs(curr-target)<abs(closest-target):
+                    closest = curr
+                if curr > target:
+                    hi -=1
+                elif curr < target:
+                    lo += 1
+                else :
+                    return curr
+        return closest  
