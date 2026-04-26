6643f40a9d32414581414cb0885d7342e4713f55
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-26 18:19:18 +0530

    Time: 473 ms (25.08%), Space: 12.3 MB (80.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,4 +1,4 @@
-class Solution(object):
+'''class Solution(object):
     def threeSumClosest(self, nums, target):
         # v can do it lil like BS 
         lenght = len(nums) 
@@ -17,4 +17,29 @@ class Solution(object):
                     lo += 1
                 else :
                     return curr
-        return closest
+        return closest'''
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        closest_sum = float('inf')
+        
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                if abs(current_sum - target) < abs(closest_sum - target):
+                    closest_sum = current_sum
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    return current_sum
+        
+        return closest_sum
