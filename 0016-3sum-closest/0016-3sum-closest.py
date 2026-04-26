4bfc15140b61dfcf17f32e2aa91bee6a8d74df6f
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-26 18:15:28 +0530

    Time: 471 ms (28.05%), Space: 12.4 MB (80.08%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -9,12 +9,12 @@ class Solution(object):
         lenght = len(nums) 
         nums.sort()
         closest = float('inf')
-        num_dict = {i: val for i, val in enumerate(nums)}
+        #num_dict = {i: val for i, val in enumerate(nums)}
         for i in range(lenght - 2):
             lo = i + 1
             hi = lenght -1
             while hi > lo:
-                curr = num_dict[hi]+num_dict[lo]+num_dict[i]
+                curr = nums[hi]+nums[lo]+nums[i]
                 if abs(curr-target)<abs(closest-target):
                     closest = curr
                 if curr > target:
