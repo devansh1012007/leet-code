8844714c36a2cee3120b410ff4532b27f0915e5f
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-04-27 22:08:49 +0530

    Time: 94 ms (6.58%), Space: 15.4 MB (29.14%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # element shuffing to the end of list 
+        i = 0
+        j=0
+        counter = len(nums)
+        if counter == 1:
+            return 1
+        while i < counter:
+            while nums[i] == nums[j] :
+                if j - i >1:
+                    nums.append(nums.pop(j)) 
+                    j -=1   
+                    counter -= 1
+                j +=1
+                if j >= counter:
+                    break 
+            i = j
+        return counter
