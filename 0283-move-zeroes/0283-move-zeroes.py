7d04111c6faa51c0d95be32e45287b86b684d620
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-05-04 00:42:22 +0530

    Time: 3 ms (90.05%), Space: 13.6 MB (19.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,9 +5,8 @@ class Solution(object):
         length = len(nums)
         if length == 1:
             return nums
-        while i < length:
+        for i in range(length):
             if nums[i] != 0:
                 nums[i],nums[j]= 0,nums[i] 
                 j +=1
-            i +=1
         return nums
