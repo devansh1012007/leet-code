95723968fb8b6122fdc9208e3594fbd6c2a85596
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-05-08 02:14:05 +0530

    Time: 0 ms (100%), Space: 12.6 MB (6.76%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -4,14 +4,21 @@
 #         self.val = val
 #         self.next = next
 
-# input(head); if head or head.next is None, return None; a = head.next ; a.next = head; a.next.next = recursive call (head.next.next); return a
-        
+# input(head); if head or head.next is None, return None; a = head.next ; head.next = recursive call (head.next.next);a.next = head; return a
+
+# 1ST try baby !!!
+
 class Solution(object):
     def swapPairs(self, head):
         if head is None or head.next is None :
             return head
         a = head.next
-        head.next = self.swapPairs(head.next.next)
-        a.next = head
+        # method 1
+        #a.next = self.swapPairs(head.next.next)
+        #head.next = self.swapPairs(head.next.next) # <-- another way
+        #a.next = head
+        # method 2
+        head.next, a.next = self.swapPairs(head.next.next), head # <-- another way
+        
         return a
         
