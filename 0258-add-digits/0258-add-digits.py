29798ce9e9c5b1932aff28b1f2637fb5c7cd297c
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-05-17 00:55:47 +0530

    Time: 0 ms (100%), Space: 12.3 MB (90.15%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def addDigits(self, num):
-        total = 0
-        for i in str(num):
-            total += int(i)
-        if total<10:return total
-        else:return self.addDigits(total)
-        
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        else:
+            return num % 9
