eb6bf024929ca59e62ef40d9de20d2a3b26ea57f
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-05-27 20:05:29 +0530

    Time: 4 ms (55.58%), Space: 19.3 MB (25.41%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -3,7 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-'''class Solution(object):
+'''
+class Solution(object):
     def removeElements(self, head, val):
         curr = head
         while curr:
@@ -11,20 +12,22 @@
                 if curr.next:
                     curr.val = curr.next.val
                     curr.next= curr.next.next 
-                    curr = curr.next
                 else: 
                     curr = None
                     return head
-        return head'''
+            curr = curr.next
+        return head
+'''
 class Solution(object):
     def removeElements(self, head, val):
         final = ListNode()
         curr = final
         while head:
-            if head.val != val: 
-                curr.next = head
+            curr.next = head
+            while head and head.val != val: 
                 curr = curr.next
-            else:
-                curr.next = None
-            head = head.next
+                head = head.next
+            curr.next = None
+            if head: head = head.next
+            else: return final.next
         return final.next
