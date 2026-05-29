2e01d78b8ecbdb37febf6f9cf0803e872d5f6e4c
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-05-30 00:20:59 +0530

    Time: 0 ms (100%), Space: 12.3 MB (94.19%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,13 +1,10 @@
-# ways of doing it :
-# reverse --> remove --> reverse
-# covert to list --> edit list --> make a new LL
-# covert only the last n+1 to list --> edit list --> make a new LL --> attach it to exiting LL
-# use 2 pointer at a sepration 'n' steps --> move then by 1 unit till the fisrt pointer reaches the end --> remove the node in front of second pointer --> return the head
-
-# imp point if 2nd pointer node is None; return second pointer.next
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
         curr=head
         second=first=curr
         for _ in range(n):
