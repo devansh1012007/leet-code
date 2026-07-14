a0661cb787aad7a6ddbb1d74d3b425f826f4c7c7
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-07-14 15:47:45 +0530

    Time: 0 ms (100%), Space: 19.3 MB (75.64%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -1,24 +1,47 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def rotateRight(self, head, k):
-        if not head or not head.next or k == 0:
-            return head
-        length = 1
+# how to solve : 
+'''
+        go to the end , count the nodes on the way 
+        divide the value of k with no. of nodes ; take the remender 
+        join the 1st and last node and then move the pointer counter-remainder number of times 
+        return self.next as head and set self.next = None 
+        '''
+        # big fuck up --> i am think rotation == pointer movemenst
+        
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         curr = head
-        while curr.next:
-            length += 1
+        counter = 1
+        if curr == None: return None
+        while curr.next: 
+            counter += 1 
             curr = curr.next
         curr.next = head
-        k %= length
-        steps_to_new_head = length - k
-        new_tail = head
-        for _ in range(steps_to_new_head - 1):
-            new_tail = new_tail.next
-
-        new_head = new_tail.next
-        new_tail.next = None
-        return new_head
+        #print(counter)
+        if counter>k:
+            k = counter-k
+            #print(k)
+            while k > 1:
+                head=head.next
+                k -= 1
+            #print(curr.val, head)
+            curr = head.next
+            head.next = None
+            return curr
+        else: 
+            k = counter - (k%counter) 
+            #print(k)
+            while k > 1:
+                head=head.next
+                k -= 1
+            #print(curr.val, head)
+            curr = head.next
+            head.next = None
+            return curr
+        #remainder=counter-remainder
+        print(remainder)
+        
