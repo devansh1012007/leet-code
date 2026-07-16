c9eb9e5441ba709f3131740f03f8d335c939467a
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-07-16 16:48:29 +0530

    Time: 3 ms (46.21%), Space: 19.2 MB (66.9%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,4 +1,5 @@
 class Solution:
+    '''
     def longestPalindrome(self, s: str) -> int:
         #if len(str) == 0 : return 0
         char = []
@@ -9,4 +10,17 @@ class Solution:
                 char.remove(i)
                 length += 2
         if len(char) >0:length += 1
+        return length
+    '''
+    def longestPalindrome(self, s: str) -> int:
+        char = {}
+        length = 0
+        odd_count = 0
+        for i in s: char[i] = char.get(i,0)+1
+        for i in char.values():
+            if i%2==0:length +=i
+            else : 
+                length +=(i-1)
+                odd_count+=1
+        if odd_count>0:length += 1
         return length
